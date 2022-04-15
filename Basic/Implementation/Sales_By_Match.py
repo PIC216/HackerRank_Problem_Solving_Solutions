e75616a98f784c061af2d9d7cccd03672b6dc160
Phoebe <101755626+PIC216@users.noreply.github.com>
2022-04-15 21:42:23 +0100

    Create Sales_By_Match.py

---
+++ Basic/Implementation/Sales_By_Match.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # takes n, the number of socks, and ar, a list of socks of different colours, and returns the number of pairs of socks, paired by colour
+    colours = list(set(ar))
+    pairs = 0
+    for colour in colours:
+        count = ar.count(colour)
+        pairs += count//2
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
